e85b905cd65b7920b587afa6df014af08436797f
almenovm <a.madiko14@gmail.com>
2019-02-08 15:35:12 +0600

    lab2 task1 and task 2

---
+++ week2/task1/task1/Program.cs
@@ -0,0 +1,42 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace task1
+{
+    class Program
+    {
+        public static string Reversem(string s)
+        {
+            char[] charArr = s.ToCharArray();
+            Array.Reverse(charArr);
+            return new string(charArr);
+        }
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader("Input.txt");
+            string s = sr.ReadToEnd();
+            sr.Close();
+            string rv = Reversem(s);
+            int cnt = 0;
+            for(int i=0; i<s.Length; i++)
+            {
+                if (s[i] == rv[i])
+                    cnt++;
+            }
+            StreamWriter sw = new StreamWriter("Output.txt");
+            if (cnt == s.Length)
+            
+                sw.WriteLine("Yes");
+                else
+                sw.WriteLine("No");
+                
+                sw.Close();
+   
+            Console.ReadKey();
+        }
+    }
+}
---
+++ week2/task2/task2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace task2
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            bool Prime(int x)
+            {
+                int crt = 0;
+                for(int i=1; i<=x; i++)
+                {
+                    if (x % i == 0)
+                        crt++;
+                }
+                if (crt == 2)
+                    return true;
+                return false;
+            }
+
+            StreamReader sr = new StreamReader("Input.txt");
+            string[] arrstr = sr.ReadToEnd().Split();
+            sr.Close();
+            int[] arr = new int[arrstr.Length];
+            for(int i=0; i<arrstr.Length; i++)
+            {
+                int x = int.Parse(arrstr[i]);
+                arr[i]= x;
+            }
+            StreamWriter sw = new StreamWriter("Output.txt");
+            for (int i=0; i<arr.Length; i++)
+            {
+                if (Prime(arr[i]))
+                    sw.Write(arr[i]+" ");
+            }
+            sw.Close();
+            Console.ReadKey();
+
+        }
+    }
+}
