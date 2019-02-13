a48e8ae2762c403acd732ce24f40984c5c21f01e
almenovm <a.madiko14@gmail.com>
2019-02-13 14:29:09 +0600

    lab2 task3,4

---
+++ week2/task3/task3/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace task3
+{
+    class Program
+    {
+
+        static void PrintSpaces(int level)
+        {
+            for (int i = 0; i < level; i++)
+            {
+
+                Console.Write("    ");
+            }
+        }
+        static void Folders(DirectoryInfo directory, int level)
+        {
+            FileInfo[] files = directory.GetFiles();
+
+            DirectoryInfo[] directories = directory.GetDirectories();
+
+            foreach (FileInfo file in files)
+            {
+                PrintSpaces(level);
+
+                Console.WriteLine(file.Name);
+            }
+
+            for (int i = 0; i < directories.Length; i++)
+            {
+                PrintSpaces(level);
+                Console.WriteLine(directories[i].Name);
+                Folders(directories[i], level + 1);
+            }
+        }
+        static void Main(string[] args)
+        {
+
+            DirectoryInfo d = new DirectoryInfo("C:/Users/Альменов/Desktop/test1");
+            Folders(d, 0);
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ week2/task4/task4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace task4
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamWriter sw = new StreamWriter(@"C: /Users/Альменов/Desktop/path/test1.txt");
+            sw.Write(111);
+            sw.Close();
+            string sourceFile = @"C:\Users\Альменов\Desktop\path\test1.txt";
+            string dest = @"C:\Users\Альменов\Desktop\path1\test1.txt";
+            File.Copy(sourceFile, dest);
+            File.Delete(sourceFile);
+        }
+    }
+}
